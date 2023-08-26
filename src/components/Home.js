c0a1961096aa565888ce24f96e7bbbfe709d96f4
MoatazSalah306 <139802667+MoatazSalah306@users.noreply.github.com>
2023-08-26 23:00:55 +0300

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,7 +9,7 @@ function Home (){
             <img className="object-cover object-center rounded" alt="hero" src={HOME_IMG} />
           </div>
           <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
-            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Before they sold
+            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">Before they sold
               <br className="hidden lg:inline-block" /> shop now and catch our offers
             </h1>
             <p className="mb-8 leading-relaxed">here in friday store we've a lot of offers make you shop now without any hesitate waiting for what let's shop now !!</p>
